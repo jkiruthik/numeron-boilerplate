ddd3fa30096e8c7e7aebe0aa0b8c56ee0d897152
kiruthik <kiruthik.j@kalvium.community>
2023-11-17 10:42:58 +0530

    commit

--- score.js
+++ score.js
@@ -1,1 +1,12 @@
-// Iteration 5: Store the player score and display it on the game over screen
+var urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
+var score = urlParams.get("score");
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+playAgainButton.onclick = (evt) => {
+  location.href = "./game.html";
+};
